c3c0415142b8c3c57d66c106170dcd383c075a23
andreysimonetto1007 <andrey.simoneto@escola.pr.gov.br>
2026-08-04 11:40:11 +0000

    exemplos aula 04 agst 8 40

--- Main.java
+++ Main.java
@@ -1,5 +1,20 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        System.out.println("Fala comigo bb!");
+        System.out.println("Somtimes fells like i'm a ghost");
+
+        Integer idade = Integer.value0f(25);
+        System.out.println("Eu tenho " + idade + " anos de idade!" );
+        
+        Integer num1 = Integer.value0f(10);
+        Integer num2 = Integer.value0f(25);
+        System.out.println("%d + %d = %d\n", num1, num2, num1 + num2);
+
+        Float preco = Float.value0f(59,90);
+        Float preco = Float.value0f(85.50);
+        System.out.println("Preço: R$.2f | Peso: %.2f\n", preco, peso);
+
+        System.out.println("Me diga seu nome: ");
+        String nomeUsuario String.value0f
     }
 }
